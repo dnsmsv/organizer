f6b9b97b8f2a8ef8d2f96b231ba1b94f9b79ac7b
dnsmsv <dnsmsv@gmail.com>
2020-09-09 16:29:38 +0300

    Realized header, added pipe for transform moment to Date format

---
+++ src/app/selector/selector.component.ts
@@ -0,0 +1,12 @@
+import { DateService } from './../shared/date.service';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-selector',
+  templateUrl: './selector.component.html',
+  styleUrls: ['./selector.component.scss']
+})
+export class SelectorComponent {
+
+  constructor(private dateService: DateService) { }
+}
